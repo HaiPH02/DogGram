4d8c146c855710d41e47b80d85eaa2e4e48d4e2e
HaiPH <haipham.vnu@gmail.com>
2024-10-28 14:52:55 +0700

    Initial Commit

---
+++ Dogy/DogyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogyApp.swift
+//  Dogy
+//
+//  Created by Hai Pham on 28/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct DogyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
