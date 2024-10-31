63de0f6d500dddb198af969f5129cdab6a2d1a49
HaiPH <haiph0208@gmail.com>
2024-10-31 17:17:51 +0700

    add ImagePicker

---
+++ Dogy/Views/Screens/UploadView.swift
@@ -0,0 +1,58 @@
+//
+//  UploadView.swift
+//  Dogy
+//
+//  Created by HaiPH on 31/10/2024.
+//
+
+import SwiftUI
+
+struct UploadView: View {
+    @State var showImagePicker: Bool = false
+    @State var imageSelected = UIImage(named: "logo")!
+    @State var sourceType: UIImagePickerController.SourceType = .camera
+    
+    var body: some View {
+        ZStack {
+            VStack(spacing: 0) {
+                Button(action: {
+                    sourceType = .camera
+                    showImagePicker.toggle()
+                }, label: {
+                    Text("Take photo".uppercased())
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.MyTheme.yellowColor)
+                })
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.MyTheme.purpleColor)
+                
+                Button(action: {
+                    sourceType = .photoLibrary
+                    showImagePicker.toggle()
+                }, label: {
+                    Text("Import phto".uppercased())
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.MyTheme.purpleColor)
+                })
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.MyTheme.yellowColor)
+            }
+            .sheet(isPresented: $showImagePicker, content: {
+                ImagePicker(imageSelected: $imageSelected, sourceType: $sourceType)
+            })
+            
+            Image("logo.transparent")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100, alignment: .center)
+                .shadow(radius: 12)
+        }
+        .edgesIgnoringSafeArea(.top)
+    }
+}
+
+#Preview {
+    UploadView()
+}
